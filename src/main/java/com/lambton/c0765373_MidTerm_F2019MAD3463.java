7794f134decd5c532e7c0ba526743d8f44a3d446
macstudent <sachinmahi>
2019-09-25 19:54:34 -0400

    done

--- src/main/java/com/lambton/c0765373_MidTerm_F2019MAD3463.java
+++ src/main/java/com/lambton/c0765373_MidTerm_F2019MAD3463.java
@@ -12,8 +12,6 @@ public class c0765373_MidTerm_F2019MAD3463
         System.out.println("1000.1 "+object.binaryToDecimal("1000.1"));
         System.out.println("James tiBeriUs kiRK " + object.initials("James tiBeriUs kiRK"));
 
-
-
     }
 
 }
