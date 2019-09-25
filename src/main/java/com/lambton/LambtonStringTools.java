039e580f6e1c639040d68b3010afe4acb8811de7
macstudent <sachinmahi>
2019-09-25 18:49:10 -0400

    reverse string checking

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -2,5 +2,17 @@ package com.lambton;
 
 public class LambtonStringTools
 {
-    
+    public static void main(String[] args)
+    {
+        String s1 = "sachin";
+        char R[] = s1.toCharArray();
+        char rev[] = new char[R.length];
+        int cnt = 0;
+        for (int i = R.length - 1; i >= 0; i--) {
+            rev[cnt++] = R[i];
+        }
+        String reverseString = new String(rev);
+        System.out.println("Reverse String = " + reverseString);
+    }
+
 }
