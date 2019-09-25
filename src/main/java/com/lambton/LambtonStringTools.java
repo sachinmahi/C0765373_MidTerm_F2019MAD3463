890b7b493e7138a1a8fd2be6d300e4d15f7b2f07
macstudent <sachinmahi>
2019-09-25 19:54:08 -0400

    done

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -1,18 +1,53 @@
 package com.lambton;
 
-public class LambtonStringTools
-{
-    public static void main(String[] args)
-    {
-        String s1 = "sachin";
-        char R[] = s1.toCharArray();
-        char rev[] = new char[R.length];
-        int cnt = 0;
-        for (int i = R.length - 1; i >= 0; i--) {
-            rev[cnt++] = R[i];
-        }
-        String reverseString = new String(rev);
-        System.out.println("Reverse String = " + reverseString);
+public class LambtonStringTools {
+    public static void main(String[] args) {
+
     }
 
+    public String reverse(String string) {
+        StringBuilder rev = new StringBuilder();
+        for (int i = string.length() - 1; i >= 0; i--) {
+            rev.append(string.charAt(i));
+        }
+        return rev.toString();
+    }
+
+    public int binaryToDecimal(String binary) {
+        int decimal = 0;
+        int binaryMultiplier = 1;
+        for (char r : reverse(binary).toCharArray()) {
+            if (r == '1') {
+                decimal += binaryMultiplier;
+            } else if (r == '0') {
+            } else {
+                return -1;
+            }
+            binaryMultiplier *= 2;
+        }
+        return decimal;
+    }
+
+    public String initials(String fullName) {
+        fullName = fullName.toUpperCase();
+        String names[] = fullName.split(" ");
+        if (names.length == 2) {
+            return null;
+        } else {
+            String initials = " ";
+            for (int i = 0; i < names.length - 1; i++) {
+                initials += names[i].toUpperCase().charAt(0) + ". ";
+            }
+            initials += titleCase(names[names.length - 1]);
+            return initials;
+        }
+    }
+
+    private String titleCase(String name) {
+        name = name.toLowerCase();
+        String titleCaseName = "";
+        titleCaseName += Character.toString(name.charAt(0)).toUpperCase();
+        titleCaseName += name.substring(1);
+        return titleCaseName;
+    }
 }
